526dbc6a31e16917949e76a0036037c7a7b5b1c8
Meet Panchal <meetpanchal190@gmail.com>
2026-05-10 12:57:54 +0530

    Add files via upload

---
+++ assignment.q.2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    signed int a = 20;
+    unsigned int b = 20;
+
+    printf("Signed integer =%d\n ",a);
+    printf("unsigned integer =%d\n ",b);
+
+    return 0;
+    
+
+}
